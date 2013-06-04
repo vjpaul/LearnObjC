cee302a990ca5e51b20f99580c9425033335cc9b
Gert-Jan <gjh@Gert-Jans-MacBook-Pro.local>
2013-06-04 09:32:57 +0200

    Initial Commit

---
+++ LearnObjC/LOCAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  LOCAppDelegate.h
+//  LearnObjC
+//
+//  Created by Gert-Jan on 6/4/13.
+//  Copyright (c) 2013 Gert-Jan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LOCAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ LearnObjC/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  LearnObjC
+//
+//  Created by Gert-Jan on 6/4/13.
+//  Copyright (c) 2013 Gert-Jan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "LOCAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([LOCAppDelegate class]));
+    }
+}
